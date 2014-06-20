08effbb2d32d679d5345d85699fcd7e3214d9ffb
Dmitry Smirnov <ohtheanguish@gmail.com>
2014-06-20 21:40:52 +0400

    Completed plots 2 and 3, fixed plot 2

--- plot2.R
+++ plot2.R
@@ -1,7 +1,7 @@
-draw.plot1 <- function() {
+draw.plot2 <- function() {
 
   # Set the graphics parameters
-  png(filename = "plot1.png",
+  png(filename = "plot2.png",
       width = 800, height = 600, units = "px", pointsize = 12,
       bg = "white", res = NA, family = "", restoreConsole = TRUE,
       type = c("windows", "cairo", "cairo-png"))
@@ -13,25 +13,27 @@ draw.plot1 <- function() {
   data$type <- factor(data$type)
   data$year <- factor(data$year)
   
-  
   # Prepare data
+  data <- data[data$fips == "24510",]
+  
   by.year <- cbind(by(data, data$year, function(y) {sum(y$Emissions)}))
   colnames(by.year) <- c("Total") 
   by.year <- data.frame(Year=rownames(by.year), by.year)
   
   # Build the plot
+  scale <- 1
   ramp <- colorRamp(c("white","#FF2500"))
   max <- max(by.year$Total)
   cols <- ramp(by.year$Total / max)
   cols <- rgb(cols[,1], cols[,2], cols[,3], maxColorValue=256)
   
-  p <- barplot(height=by.year$Total/1000000, names.arg=by.year$Year, col=cols, 
-               ylim=c(0, max*1.1/1000000))
+  p <- barplot(height=by.year$Total/scale, names.arg=by.year$Year, col=cols, 
+               ylim=c(0, max*1.1/scale))
 
-  text(p, 2, format(by.year$Total/1000000, digits = 3), xpd=TRUE, col="black")  
-  abline(lm(by.year$Total/1000000 ~ p), col="darkgreen", lwd=3)
+  abline(lm(by.year$Total ~ p), col="darkgreen", lwd=3)
   
-  title(expression("Total Emissions of" ~ PM[2.5] ~ "(millions of tons)"))  
+  text(p, 1000, format(by.year$Total, digits = 3), xpd=TRUE, col="black")  
+  title(expression("Total Emissions of" ~ PM[2.5] ~ "in Baltimore, MA (tons)"))  
   
   # Finish up.
   dev.off()
---
+++ plot3.R
@@ -0,0 +1,29 @@
+library("ggplot2")
+
+draw.plot3 <- function() {
+
+  # Read data
+  data <- readRDS("summarySCC_PM25.rds")
+  data$type <- factor(data$type)
+  data$year <- factor(data$year)
+  
+  # Prepare data
+  data <- data[data$fips == "24510",]
+  
+  by.year.type <- aggregate(data$Emissions, list(Year=data$year, Type=data$type), sum)
+  colnames(by.year.type)[3] <- "Total"
+  
+  # Build the plot
+  p <- ggplot(by.year.type) + aes(factor(Year), Total) + 
+  geom_bar(stat="identity") + 
+  stat_smooth(method="lm", se=FALSE, size=1.5, alpha=0.8, color="darkgreen", 
+              mapping=aes(factor(Year), Total, group = 1)) + 
+  facet_wrap(~Type, nrow=2, ncol=2) + 
+  xlab("Year") + 
+  ylab("") + 
+  ggtitle(expression("Emissions of" ~ PM[2.5] ~ "by Source Type (tons)")) + 
+  geom_text(data=by.year.type, aes(label=format(Total, digits=3)), 
+            position=position_dodge(width=0.9), vjust=-0.35, size=4)  
+
+  ggsave(file="plot3.png")
+}
