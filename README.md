2e9bbf8b2a7b0b2f10e4772bb687bd952e211055
Dmitry Smirnov <ohtheanguish@gmail.com>
2014-06-21 02:06:10 +0400

    Finished plot 6, working on README

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-ExData_Plotting2
+Programming Assignment II for the "Exploratory Data Analysis" Course
 ================
 
-Programming Assignment II for the "Exploratory Data Analysis" Course
+## Question 1 ##
+
+Have total emissions from PM2.5 decreased in the United States from 1999 to 2008? 
+Using the base plotting system, make a plot showing the total PM2.5 emission from all sources for each of the years 1999, 2002, 2005, and 2008.
+
+![Plot 1](/plot1.png "Plot 1")
+
+See [plot1.R](/plot1.R) for code.
--- plot6.R
+++ plot6.R
@@ -5,38 +5,40 @@ draw.plot6 <- function() {
   # Read data
   data <- readRDS("summarySCC_PM25.rds")
   cls <- readRDS("Source_Classification_Code.rds")
-  data <- data[data$fips == "24510",]
+  data <- data[data$fips == "24510" | data$fips == "06037",]
   data$type <- factor(data$type)
   data$year <- factor(data$year)
   
   # Prepare data
+  data[data$fips == "24510", "Location"] <- "Baltimore"
+  data[data$fips == "06037", "Location"] <- "LA County"
+  
   cls.veh <- subset(cls, grepl("veh", cls[["EI.Sector"]], 
                                 ignore.case=TRUE), select=c(SCC, EI.Sector, SCC.Level.One, SCC.Level.Two))
   data <- merge(data, cls.veh, by=c("SCC"))
-  by.year.type <- aggregate(data$Emissions, list(Year=data$year, Type=data$SCC.Level.Two), sum)
-  colnames(by.year.type)[3] <- "Total"
-  by.year.type$Total = by.year.type$Total
-  
-  # Squash all misc categories and re-aggregate
-  by.year.type$Type <- as.character(by.year.type$Type)  
-  by.year.type[by.year.type$Total < 1, "Type"] <- "Other"
-  
-  by.year.type$Type <- factor(by.year.type$Type)  
-  by.year.type <- aggregate(by.year.type$Total, list(Year=by.year.type$Year, 
-                                                     Type=by.year.type$Type), sum)
-  colnames(by.year.type)[3] <- "Total"  
+  by.year.type <- aggregate(data$Emissions, list(Year=data$year, Type=data$SCC.Level.Two, 
+                                                 Location=data$Location), sum)
+  colnames(by.year.type)[4] <- "Total"
   
   # Build the plot
   p <- ggplot(by.year.type) + aes(factor(Year), Total) + 
-  geom_bar(stat="identity") + 
-  stat_smooth(method="lm", se=FALSE, size=1.5, alpha=0.8, color="darkgreen", 
-              mapping=aes(factor(Year), Total, group = 1)) + 
-  facet_wrap(~Type, ncol=2) + 
-  xlab("Year") + 
-  ylab("") + 
-  ggtitle(expression("Emissions of" ~ PM[2.5] ~ "in Baltimore, MA From Vehicle Sources (tons)")) + 
-  geom_text(data=by.year.type, aes(label=format(Total, digits=2)), 
-            position=position_dodge(width=0.9), vjust=-0.35, size=4)  
+    geom_bar(aes(fill=Location), stat="identity", position="dodge") + 
+    stat_smooth(data=by.year.type[by.year.type$Location == "LA County",], 
+                method="lm", se=FALSE, size=1, alpha=0.8, color="darkgreen", 
+                mapping=aes(factor(Year), Total, group = 1)) + 
+    stat_smooth(data=by.year.type[by.year.type$Location == "Baltimore",], 
+                method="lm", se=FALSE, size=1, alpha=0.8, color="darkred", 
+                mapping=aes(factor(Year), Total, group = 1)) + 
+    facet_wrap(~Type, ncol=2) + 
+    xlab("Year") + 
+    ylab("") + 
+    ggtitle(expression("Emissions of" ~ PM[2.5] ~ " From Vehicle Sources (tons)")) + 
+    geom_text(data=by.year.type[by.year.type$Location == "Baltimore",], 
+              aes(label=format(Total, digits=2)), position=position_dodge(width=0.9), 
+              vjust=-0.35, hjust=1.4, size=4, colour=c("Black")) + 
+    geom_text(data=by.year.type[by.year.type$Location == "LA County",], 
+              aes(label=format(Total, digits=2)), position=position_dodge(width=0.9), 
+              vjust=-0.35, hjust=0, size=4, colour=c("darkgrey"))
 
   ggsave(file="plot6.png")
 }
